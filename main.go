166463a30e4fe74f281acad67df58b4a0f833159
Nate <big.nate.w@gmail.com>
2018-06-22 23:01:04 -0600

    Initial prototype

---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+var (
+	addr = flag.String("addr", ":8080", "ui address")
+	api  = flag.String("api", "http://localhost:5000", "api address")
+	tout = flag.Duration("tout", time.Second, "api cache timeout")
+)
+
+func main() {
+	flag.Parse()
+	println("Serving on " + *addr)
+	http.ListenAndServe(*addr, &memory{
+		tout: time.Now().Add(-*tout),
+		data: func(w http.ResponseWriter, r *http.Request) {
+			http.Error(w, "pending first load", http.StatusOK)
+		},
+	})
+}
+
+var htmlTPL = template.Must(template.New("").Parse(`<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Registry Listing</title>
+  </head>
+  <body>
+		{{range $name, $tags := . -}}
+			<h2>{{$name}}</h2>
+			<ul>
+				{{range $tags -}}
+					<li>{{.}}</li>
+				{{end}}
+			</ul>
+		{{else}}<p>No Repositories Found</p>{{end}}
+  </body>
+</html>`))
+
+type memory struct {
+	data http.HandlerFunc
+	tout time.Time
+}
+
+func (mem *memory) update() error {
+	res, err := http.Get(*api + "/v2/_catalog")
+	if err != nil {
+		return err
+	}
+	// TODO: check status
+	var catalog struct {
+		Repos []string `json:"repositories"`
+	}
+	if err := json.NewDecoder(res.Body).Decode(&catalog); err != nil {
+		return err
+	}
+	if err := res.Body.Close(); err != nil {
+		return err
+	}
+
+	data := make(map[string][]string, len(catalog.Repos))
+
+	for _, name := range catalog.Repos {
+		res, err := http.Get(*api + "/v2/" + name + "/tags/list")
+		if err != nil {
+			return err
+		}
+		// TODO: check status
+		var tags struct {
+			Name string   `json:"name"`
+			Tags []string `json:"tags"`
+		}
+		if err := json.NewDecoder(res.Body).Decode(&tags); err != nil {
+			return err
+		}
+		data[name] = tags.Tags
+		if err := res.Body.Close(); err != nil {
+			return err
+		}
+	}
+
+	mem.data = func(w http.ResponseWriter, r *http.Request) {
+		if err := htmlTPL.Execute(w, data); err != nil {
+			println("causing failure " + err.Error())
+		}
+	}
+	return nil
+}
+
+func (mem *memory) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
+	if now.After(mem.tout) {
+		mem.tout = now.Add(*tout)
+		if err := mem.update(); err != nil {
+			mem.data = func(w http.ResponseWriter, r *http.Request) {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}
+	}
+	w.Header().Add("X-BIGN8-TIMEOUT", mem.tout.String())
+	w.Header().Add("X-BIGN8-NOW", now.String())
+	mem.data(w, r)
+}
